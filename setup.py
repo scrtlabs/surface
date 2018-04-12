45cb6a4c80b72348d22fd0113b00d8823f80f5df
Elichai Turkel <elichai@enigma.co>
2018-04-12 19:21:38 +0300

    DEV: Add a setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import io
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name='surface',
+    version='',
+    description='',
+    long_description=long_description,
+    package_dir={'': 'src'},
+    packages=find_packages('src', exclude=('tests', )),
+    url='',
+    license='',
+    author='',
+    author_email='',
+)

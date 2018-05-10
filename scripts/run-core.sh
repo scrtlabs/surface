7e69a6a97bc7288105e92869a4121711c3c70437
Frederic Fortier <fredfortier@gmail.com>
2018-05-10 11:48:45 -0700

    BLD: fixed core docker image and evm build script

--- scripts/run-core.sh
+++ scripts/run-core.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --net enigmanet --ip 172.75.5.10 --name core mvp0/core /bin/bash
+docker run -it --rm --net enigmanet --ip 172.75.5.10 -p 1338:1338 --name core mvp0/core /bin/bash
